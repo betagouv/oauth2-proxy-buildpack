6bba7549519ab8db25108f0573ac3a4b3642f4d8
Julien Bouquillon <julien@bouquillon.com>
2025-12-30 15:20:00 +0100

    fix: handle all OAUTH2_PROXY_* env vars

--- scripts/start_static_oauth2_proxy.sh
+++ scripts/start_static_oauth2_proxy.sh
@@ -10,26 +10,11 @@ if [ -z ${OAUTH2_PROXY_CLIENT_ID+x} ]; then echo "please set OAUTH2_PROXY_CLIENT
 if [ -z ${OAUTH2_PROXY_CLIENT_SECRET+x} ]; then echo "please set OAUTH2_PROXY_CLIENT_SECRET"; exit 1; fi
 if [ -z ${OAUTH2_PROXY_COOKIE_SECRET+x} ]; then echo "please set OAUTH2_PROXY_COOKIE_SECRET"; exit 1; fi
 
-OAUTH2_PROXY_SET_XAUTHREQUEST="${OAUTH2_PROXY_SET_XAUTHREQUEST:-true}"
-export OAUTH2_PROXY_SET_XAUTHREQUEST
-
-OAUTH2_PROXY_PASS_ACCESS_TOKEN="${OAUTH2_PROXY_PASS_ACCESS_TOKEN:-true}"
-export OAUTH2_PROXY_PASS_ACCESS_TOKEN
-
-OAUTH2_PROXY_HTTP_ADDRESS="${OAUTH2_PROXY_HTTP_ADDRESS:-http://:$PORT}"
-export OAUTH2_PROXY_HTTP_ADDRESS
-
-if [ -n "${OAUTH2_EMAIL_DOMAIN}" ]; then
-    OAUTH2_PROXY_EMAIL_DOMAINS="${OAUTH2_EMAIL_DOMAIN}"
-else
-    OAUTH2_PROXY_EMAIL_DOMAINS="*"
-fi
-export OAUTH2_PROXY_EMAIL_DOMAINS
-
-if [ -n "${OAUTH2_GITHUB_ORG}" ]; then
-    OAUTH2_PROXY_GITHUB_ORG="${OAUTH2_GITHUB_ORG}"
-    export OAUTH2_PROXY_GITHUB_ORG
-fi
+while IFS='=' read -r name value; do
+    if [[ $name == OAUTH2_PROXY_* ]]; then
+        export "$name"
+    fi
+done < <(env)
 
 echo "starting oauth2-proxy..."
 exec ./oauth2-proxy --upstream file:///app/dist/#/
